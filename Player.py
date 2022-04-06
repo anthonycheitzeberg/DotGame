ae1967ced63a77aa840d46964f0f32c2e03513f1
schel <robertjschelling@su.edu.ph>
2022-04-06 19:15:25 +0800

    ## Player File: <!-- Added Player File with PlayerSettings and PlayerStats as well as a Vector File -->

---
+++ Player.py
@@ -0,0 +1,21 @@
+class Player:
+    def __init__(self, *args, **kwargs):
+        self.id = kwargs.get("id", '')
+        self.pos = kwargs.get("pos", [])
+        self.settings - kwargs.get("settings", PlayerSettings())
+        self.stats = kwargs.get("stats", PlayerStats())
+
+    def move(self, vector: Vector):
+        self.pos = [self.pos[0] + vector.x * (vector.speed * stats.movement_speed),
+                    self.pos[1] + vector.y * (vector.speed * stats.movement_speed)]
+
+
+class PlayerSettings:
+    def __init__(self, *args, **kwargs):
+        self.username = kwargs.get("username", "Player")
+        self.color = kwargs.get("color", [0, 0, 0])
+
+
+class PlayerStats:
+    def __init__(self, *args, **kwargs):
+        self.movement_speed = kwargs.get("speed", 0.5)
