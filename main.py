3d34973baa395f6bfafd961ef686175ddf3e0de5
anthonycheitzeberg <a.heitzeberg007@gmail.com>
2022-04-07 10:42:03 +0800

    add init lines in main

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 
+clock = pygame.time.Clock()
+
+pygame.init()
+
+pygame.display.set_caption("My Pygame Window")
+
+WINDOW_SIZE = (400, 400)
+
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
