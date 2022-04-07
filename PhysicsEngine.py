5cc9ac125f7398424018779051983b8bedae3d78
schel <robertjschelling@su.edu.ph>
2022-04-07 17:21:04 +0800

    ## Bullets: <!-- Added basic bullets and their physics -->

---
+++ PhysicsEngine.py
@@ -0,0 +1,10 @@
+from Globals import SCREEN_WIDTH, SCREEN_HEIGHT
+import math
+
+
+def is_out_of_bounds_circle(x, y, r):
+    return x < 0 or x > SCREEN_WIDTH or y < 0 or y > SCREEN_HEIGHT or x + (r*math.pi) > SCREEN_WIDTH or y + (r*math.pi) > SCREEN_HEIGHT
+
+
+def is_out_of_bounds_rect(x, y, width, height):
+    return x < 0 or x > SCREEN_WIDTH or y < 0 or y > SCREEN_HEIGHT or x + width > SCREEN_WIDTH or y + height > SCREEN_HEIGHT
